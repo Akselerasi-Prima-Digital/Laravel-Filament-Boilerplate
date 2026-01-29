0c7272da43685e544487dc0bbee0a66f8b1bd3e3
Reynaldi Arya <reynaldiarya.id@gmail.com>
2026-01-29 13:06:00 +0700

    Filament

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $superAdmin = User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('admin1234'),
+        ]);
+
+        $superAdmin->assignRole('Super Admin');
+    }
+}
